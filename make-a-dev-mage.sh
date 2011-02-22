a8777867abd75caf6a43da962549a6e450fb7320
Benjamin <bc@thermitic.net>
2011-02-22 13:06:07 -0500

    initial try on mage dev automation

---
+++ make-a-dev-mage.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# commit any changes before doing this, for ease of use
+
+PREFIX="mage_"
+
+SRC_DBFILE="/path/to/dbdump.sql"
+SRC_DBUSER="livedbuser"
+SRC_DBPASS="livedbpass"
+SRC_DBNAME="livedbuser_dbname"
+
+DST_URL="http://dev.site.name/"
+DST_DBUSER="devuser"
+DST_DBPASS="devpass"
+DST_DBNAME="devuser_dbname"
+SRC_DIR="/path/to/livemage"
+DST_DIR="/path/to/devmage"
+
+TESTING_MAIL="user@example.com"
+
+SEED=$(dd if=/dev/urandom  bs=1 count=11 status=noxfer 2>/dev/null|md5sum)
+RAND=${SEED:0:8}
+
+rm -rf $DST_DIR
+rsync -a $SRC_DIR/ $DST_DIR/
+echo "drop database $DST_DBNAME" | mysql -u $DST_DBUSER -p$DST_DBPASS
+echo "create database $DST_DBNAME" | mysql -u $DST_DBUSER -p$DST_DBPASS
+mysql  -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME < $SRC_DBFILE
+rm -rf $DST_DIR/var/cache/*
+rm -rf $DST_DIR/var/session/*
+echo "update ${PREFIX}core_config_data set value='$DST_URL' where path like 'web/%/base_url';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
+echo "update ${PREFIX}core_config_data set value='$TESTING_MAIL' where value like '%@%.com';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
+echo "update ${PREFIX}core_config_data set value='$TESTING_MAIL' where value like '%@%.org';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
+echo "update ${PREFIX}core_config_data set value='$TESTING_MAIL' where value like '%@%.net';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
+sed -i "s/<prefix>[a-zA-Z]*<\/prefix>/<prefix>$DST_DBNAME<\/prefix>/" $DST_DIR/app/etc/local.xml
+sed -i "s/<username><\!\[CDATA\[$SRC_DBUSER\]\]><\/username>/<username><![CDATA[$DST_DBUSER]]><\/username>/" $DST_DIR/app/etc/local.xml
+sed -i "s/<password><\!\[CDATA\[$SRC_DBPASS\]\]><\/password>/<password><![CDATA[$DST_DBPASS]]><\/password>/" $DST_DIR/app/etc/local.xml
+sed -i "s/<dbname><\!\[CDATA\[$SRC_DBNAME\]\]><\/dbname>/<dbname><![CDATA[$DST_DBNAME]]><\/dbname>/" $DST_DIR/app/etc/local.xml
+
