bc8c9753ceb1127ad2005143147eb12c6dd7fef4
Benjamin <bc@thermitic.net>
2011-02-22 16:43:15 -0500

    authorize.net debug and test

--- make-a-dev-mage.sh
+++ make-a-dev-mage.sh
@@ -15,6 +15,9 @@ DST_DBNAME="devuser_dbname"
 SRC_DIR="/path/to/livemage"
 DST_DIR="/path/to/devmage"
 
+TESTING_AUTHNET_LOGIN=""
+TESTING_AUTHNET_TRANSKEY=""
+
 TESTING_MAIL="user@example.com"
 
 SEED=$(dd if=/dev/urandom  bs=1 count=11 status=noxfer 2>/dev/null|md5sum)
@@ -31,6 +34,12 @@ echo "update ${PREFIX}core_config_data set value='$DST_URL' where path like 'web
 echo "update ${PREFIX}core_config_data set value='$TESTING_MAIL' where value like '%@%.com';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
 echo "update ${PREFIX}core_config_data set value='$TESTING_MAIL' where value like '%@%.org';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
 echo "update ${PREFIX}core_config_data set value='$TESTING_MAIL' where value like '%@%.net';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
+
+echo "update ${PREFIX}core_config_data set value='1' where path like 'payment/authorizenet/debug';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
+echo "update ${PREFIX}core_config_data set value='1' where value like 'payment/authorizenet/test';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
+echo "update ${PREFIX}core_config_data set value='$TESTING_AUTHNET_LOGIN' where value like 'payment/authorizenet/login';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
+echo "update ${PREFIX}core_config_data set value='$TESTING_AUTHNET_TRANSKEY' where value like 'payment/authorizenet/transkey';" | mysql -u $DST_DBUSER -p$DST_DBPASS $DST_DBNAME
+
 sed -i "s/<prefix>[a-zA-Z]*<\/prefix>/<prefix>$DST_DBNAME<\/prefix>/" $DST_DIR/app/etc/local.xml
 sed -i "s/<username><\!\[CDATA\[$SRC_DBUSER\]\]><\/username>/<username><![CDATA[$DST_DBUSER]]><\/username>/" $DST_DIR/app/etc/local.xml
 sed -i "s/<password><\!\[CDATA\[$SRC_DBPASS\]\]><\/password>/<password><![CDATA[$DST_DBPASS]]><\/password>/" $DST_DIR/app/etc/local.xml
